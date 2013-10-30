cc9e48effc18345251ac6045cc96ae0d548b151e
Marco - Andrea Buchmann <marco.andrea.buchmann@gmail.com>
2013-10-30 21:06:56 +0100

    Now loading from new generation directories

--- pMSSMScanner/NewGeneration/StoreThisPoint.C
+++ pMSSMScanner/NewGeneration/StoreThisPoint.C
@@ -2,17 +2,17 @@
 #include <fstream>
 #include <stdlib.h>
 
-#include "../StorageLibrary.C"
+#include "src/StorageLibrary.cc"
 
 
-void StoreThisPoint(float compatibility) {
+void StoreThisPoint(float compatibility, float KSP_LM, float KSP_HM, float KSP_MET, float KSP_BTag, float eff ) {
   std::cout << "Storing this point with a compatibility of " << compatibility << std::endl;
   std::ifstream QuickConfig;
   QuickConfig.open("LastPoint.cfg");
   float pMSSMpars[20];
   float HiggsMass;
   while(QuickConfig >> pMSSMpars[0] >> pMSSMpars[1] >> pMSSMpars[2] >> pMSSMpars[3] >> pMSSMpars[4] >> pMSSMpars[5] >> pMSSMpars[6] >> pMSSMpars[7] >> pMSSMpars[8] >> pMSSMpars[9] >> pMSSMpars[10] >> pMSSMpars[11] >> pMSSMpars[12] >> pMSSMpars[13] >> pMSSMpars[14] >> pMSSMpars[15] >> pMSSMpars[16] >> pMSSMpars[17] >> pMSSMpars[18] >> pMSSMpars[19] >> HiggsMass) {
-    StorePoint(pMSSMpars,1,HiggsMass,1,1,1,1,1,1,1,1,1,1,compatibility);
+    StorePoint(pMSSMpars,1,HiggsMass,1,1,1,1,1,1,1,1,1,1,compatibility,KSP_LM,KSP_HM,KSP_MET,KSP_BTag,eff);
     break;
   }
 }
@@ -40,11 +40,19 @@ void StoreBestPoint(float compatibility) {
 
 
 int main(int argc, char** argv) {
-  if(argc<2) {
-    std::cout << "you're supposed to give me the compatibility so i can store it. try again. " << std::endl;
+  if(argc<3) {
+    std::cout << "you're supposed to give me the compatibility so i can store it (the second argument is the path to the summary file). try again. " << std::endl;
     return -1;
   }
   
-  StoreThisPoint(atof(argv[1]));
+    std::ifstream KS;
+    KS.open(argv[2]);
+    float KSP_LM,KSP_HM,KSP_MET,KSP_BTag,eff;
+    while(KS >> KSP_LM >> KSP_HM >> KSP_MET >> KSP_BTag >> eff) {
+        std::cout << "Loaded values " << KSP_LM << " : " << KSP_HM << " : " << KSP_MET << " : " << KSP_BTag << " : " << eff << std::endl;
+    }
+
+    
+  StoreThisPoint(atof(argv[1]),KSP_LM,KSP_HM,KSP_MET,KSP_BTag,eff);
   StoreBestPoint(atof(argv[1]));
 }
