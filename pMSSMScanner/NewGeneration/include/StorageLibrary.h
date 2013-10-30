3f4d8c772d2f3dfcc0cf99367346d144375a8da4
Marco - Andrea Buchmann <marco.andrea.buchmann@gmail.com>
2013-10-30 21:07:22 +0100

    Updated storage library to also include agreement criteria

--- pMSSMScanner/NewGeneration/include/StorageLibrary.h
+++ pMSSMScanner/NewGeneration/include/StorageLibrary.h
@@ -9,7 +9,12 @@ void StorePoint(float pMSSMpars[],
                 int SLHAValid=0, // first quick check (tachyons, charged LSP and so on)
                 float HiggsMass=-1, // second quick check (is Higgs mass usable? here the Higgs mass, rejected or not, is provided)
                 int Satisfies_bsgamma=-1,int Satisfies_gm2=-1,int Satisfies_Bsmumu=-1,int Satisfies_Higgs=-1,int Satisfies_SUSY=-1,int Satisfies_LSP=-1,int Satisfies_Top=-1,int Satisfies_Mbmb=-1,int Satisfies_Alphas=-1,int Satisfies_RBtaunu=-1, // extensive constraint check
-                float Compatibility=-1) // last test
+                float Compatibility=-1, // overall compatibility
+                float KSP_LM=-1,  // Kolmogorov-Smirnov probability for low mass test
+                float KSP_HM=-1,  // Kolmogorov-Smirnov probability for high mass test
+                float KSP_MET=-1,  // Kolmogorov-Smirnov probability for MET test
+                float KSP_BTag=-1,  // Kolmogorov-Smirnov probability for btag test
+                float eff=-1) // last test
 ;
 
 
--- pMSSMScanner/NewGeneration/src/StorageLibrary.cc
+++ pMSSMScanner/NewGeneration/src/StorageLibrary.cc
@@ -7,7 +7,12 @@ void StorePoint(float pMSSMpars[],
                 int SLHAValid, // first quick check (tachyons, charged LSP and so on)
                 float HiggsMass, // second quick check (is Higgs mass usable? here the Higgs mass, rejected or not, is provided)
                 int Satisfies_bsgamma,int Satisfies_gm2,int Satisfies_Bsmumu,int Satisfies_Higgs,int Satisfies_SUSY,int Satisfies_LSP,int Satisfies_Top,int Satisfies_Mbmb,int Satisfies_Alphas,int Satisfies_RBtaunu, // extensive constraint check
-                float Compatibility) // last test
+                float Compatibility, // overall compatibility
+                float KSP_LM,  // Kolmogorov-Smirnov probability for low mass test
+                float KSP_HM,  // Kolmogorov-Smirnov probability for high mass test
+                float KSP_MET,  // Kolmogorov-Smirnov probability for MET test
+                float KSP_BTag,  // Kolmogorov-Smirnov probability for btag test
+                float eff) // last test
 {
     std::ofstream PointLog;
     PointLog.open("pointlog.txt",std::ios::app);
@@ -28,5 +33,10 @@ void StorePoint(float pMSSMpars[],
     
     PointLog << Compatibility << " ";
     
+    PointLog << KSP_LM << " ";
+    PointLog << KSP_HM << " ";
+    PointLog << KSP_MET << " ";
+    PointLog << KSP_BTag << " ";
+    PointLog << eff << " ";
     PointLog << std::endl;
 }
